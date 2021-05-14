832d44e1924bf71cafd6f98a66c6f5b401eafc92
Thomas Vu <thomas.vu@hotmail.com>
2021-05-15 01:39:27 +0200

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,17 @@
+import "./topbar.scss";
+
+export default function topbar() {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            ThomasVu
+          </a>
+          <div className="itemContainer"></div>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+}
