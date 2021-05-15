2bf61e05328663cb841bc5432537e91f327ca462
Thomas Vu <thomas.vu@hotmail.com>
2021-05-15 17:34:59 +0200

    layout finished

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,30 @@
 import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function topbar() {
+export default function topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
             ThomasVu
           </a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+47 900 67 715</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>thomas.vu@hotmail.com</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
